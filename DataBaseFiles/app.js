a9d0a4b6c54852047948c5ef1c42be8faf6bcdaf
Kieran Zylstra <kieran.zylstra@colorado.edu>
2020-03-28 19:33:18 -0600

    The api now works with the database and the test code is in app.js

--- DataBaseFiles/app.js
+++ DataBaseFiles/app.js
@@ -8,12 +8,13 @@ client.connect();
 var ret = client.query("select * from daily_weather");
 
 var http = require("http");
-url = "http://api.openweathermap.org/data/2.5/weather?q=boulder,colorado&appid=" + apiKey;
+url = "http://api.openweathermap.org/data/2.5/weather?q=boulder,colorado&units=imperial&appid=" + apiKey;
+
 
 var request = http.get(url, function (response) {
 	
-	var buffer = "";
-
+	var buffer = "",
+		data;
 	response.on("data", function (chunk) {
 		buffer += chunk;
 	});
@@ -22,6 +23,40 @@ var request = http.get(url, function (response) {
 
 		console.log(buffer);
 		console.log("\n");
-	});
+		data = JSON.parse(buffer);
 
+		var date = new Date();
+		var temp_f_high = data.main.temp_max;
+		var temp_c_high = (temp_f_high - 32) * (9/5);
+		var temp_f_low = data.main.temp_min;
+		var temp_c_low = (temp_c_low - 32) *(9/5);
+
+		var humidity = data.main.humidity;
+		var precipitation = data.weather[0]["main"];
+		var wind = data.wind["speed"];
+		var feels_like = data.main.feels_like;
+		var pressure = data.main.pressure;
+		var description = data.weather["description"];
+debugger
+		console.log(data);
+		
+		client.query("INSERT INTO daily_weather(day, temp_f_high, temp_f_low, temp_c_high, temp_c_low, humidity, precipitation, coverage, alerts, feels_like, pressure, wind, description, precip_chance) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)",
+			[
+				date
+				,temp_f_high
+				,temp_c_high
+				,temp_f_low
+				,temp_c_low
+				,humidity
+				,precipitation
+				,null
+				,null
+				,feels_like
+				,pressure
+				,wind
+				,description
+				,null
+			]);
+	});
 });
+
